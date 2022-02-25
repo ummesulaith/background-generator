ccfdae2c6911a348e530b9471a9d8f90d7512b61
Umme Suleyath <ummesul76@gmail.com>
2022-02-25 19:03:54 +0530

    Feature: Implmented background color generator

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('This is a testing connectivity')
+let css = document.querySelector('h3');
+let color1 = document.querySelector('.color1')
+let color2 = document.querySelector('.color2')
+let body = document.getElementById('gradient')
+
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right, "+color1.value + "," + color2.value +")";
+    css.textContent = body.style.background + ";";
+}
+color1.addEventListener('input',setGradient)
+color2.addEventListener('input',setGradient)
+
+
