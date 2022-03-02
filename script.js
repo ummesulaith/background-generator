12f9e5b6eb308da4a9a2ce36e4e799b602724967
Umme Suleyath <ummesul76@gmail.com>
2022-03-02 19:57:29 +0530

    Refactor: implemented gradient background color

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let color1 = document.querySelector('.color1')
 let color2 = document.querySelector('.color2')
 let body = document.getElementById('gradient')
 
+css.innerText = window.getComputedStyle(body).background
 
 function setGradient(){
     body.style.background = "linear-gradient(to right, "+color1.value + "," + color2.value +")";
